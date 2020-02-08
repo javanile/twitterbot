d8e4b5deeb41fa118caa4a3285d357287b2ad20d
Francesco Bianco <info.francescobianco@gmail.com>
2020-02-08 13:17:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key: 'blah',
-  consumer_secret: 'blah',
-  access_token: 'blah',
-  access_token_secret: 'blah'
+  consumer_key: process.env.TWITTERBOT_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTERBOT_CONSUMER_SECRET,
+  access_token: process.env.TWITTERBOT_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTERBOT_ACCESS_TOKEN_SECRET
 }
